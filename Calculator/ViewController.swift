5c22e9dff45e192f1f2f546d2c9676b9010b8bc8
Danil <keepfocused@users.noreply.github.com>
2017-10-28 16:05:21 +0300

    Added new method
    Added new attributes & methods

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        print(sender.titleLabel?.text)
+        //print("touch digit was called")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
